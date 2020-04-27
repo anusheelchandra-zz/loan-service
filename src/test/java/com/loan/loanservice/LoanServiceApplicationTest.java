aadbae3334ed424893179561ad694bfd027f0cb8
Anusheel Chandra <anusheel.chandra@goeuro.com>
2020-04-27 13:35:15 +0200

    chore: added test foe context loading

--- src/test/java/com/loan/loanservice/LoanServiceApplicationTest.java
+++ src/test/java/com/loan/loanservice/LoanServiceApplicationTest.java
@@ -1,11 +1,22 @@
 package com.loan.loanservice;
 
+import com.loan.loanservice.controller.LoanController;
+import com.loan.loanservice.service.LoanPlanGeneratorService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LoanServiceApplicationTest {
 
+  @Autowired private LoanController loanController;
+
+  @Autowired private LoanPlanGeneratorService generatorService;
+
   @Test
-  void contextLoads() {}
+  public void shouldLoadContext() {
+    Assertions.assertThat(loanController).isNotNull();
+    Assertions.assertThat(generatorService).isNotNull();
+  }
 }
